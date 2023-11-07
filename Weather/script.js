2a9ac772406e873776adff94548b39702a5f2697
HasiukI <136508423+HasiukI@users.noreply.github.com>
2023-11-07 12:58:27 +0200

    update card

--- Weather/script.js
+++ Weather/script.js
@@ -28,35 +28,48 @@ let data= GetWeather();
 
 function push_item(data){
     const mainObject= data;
-    // console.log(mainObject);
+    const DaysInWeek=["Понеділок","Вівторок","Середа","Четвер","П'ятниця","Субота","Неділя"];
+    const Weather=[];
+    console.log(mainObject);
     const days_card =document.querySelectorAll('.days');
     const min_card =document.querySelectorAll('.min');
     const max_card =document.querySelectorAll('.max');
+    const days_name_card= document.querySelectorAll('.days_name');
+
     let card_count=0;
 
     let daystr = mainObject['list'][0]['dt_txt'].split(' ')[0];
     let numbers_temp =[];
+
+    let number_img=["рвані хмари","уривчасті хмари","легкий дощ","помірний дощ","хмарно","чисте небо","кілька хмар"];
+
     for(let i=0; i<mainObject['list'].length;i++){
 
+        console.log(mainObject['list'][i]['weather'][0]['description'])
         if(daystr !== mainObject['list'][i]['dt_txt'].split(' ')[0] || i===mainObject['list'].length-1 ){
-            console.log(daystr);
+
+
             days_card[card_count].innerText=daystr.split('-')[2];
+            days_name_card[card_count].innerText=DaysInWeek[new Date(Date.parse(daystr)).getDay()-1];
 
             numbers_temp.sort();
             min_card[card_count].innerText=numbers_temp[0];
             max_card[card_count++].innerText=numbers_temp[numbers_temp.length-1];
+            // min_card[card_count].innerText=Math.min(numbers_temp);
+            // max_card[card_count++].innerText=Math.max(numbers_temp);
             numbers_temp=[];
 
             daystr = mainObject['list'][i]['dt_txt'].split(' ')[0];
+
         }else{
-            console.log(mainObject['list'][i]['main']['temp']);
+            // console.log(mainObject['list'][i]['main']['temp']);
             numbers_temp.push(mainObject['list'][i]['main']['temp'])
         }
 
 
     }
+}
 
-
-
-
+function change_wither(numbers, card_img){
+    
 }
