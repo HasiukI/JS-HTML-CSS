bebd2115208a913f0f5b54b4a737c01ec48a9aca
HasiukI <136508423+HasiukI@users.noreply.github.com>
2023-11-10 22:58:27 +0200

    Create
    This program i use animated Hamburger menu abd drag and drop element

--- HamburgerMenu_Design/script.js
+++ HamburgerMenu_Design/script.js
@@ -3,7 +3,8 @@ let onclick=false;
 let hamburger= document.getElementById('hamburger');
 let Card = document.querySelector('.card');
 const Mains =document.querySelector('.mains');
-
+let dragItems=document.querySelectorAll('.items');
+let curentDragItem;
 // window.onload(()=>{
 //     onclick=false;
 //     Modal.style.top='50px';
@@ -79,3 +80,57 @@ function animation(){
         onclick=false;
     }
 }
+
+function handleDragStart(e){
+    curentDragItem=this;
+
+    e.dataTransfer.effectAllowed='move';
+     e.dataTransfer.setData('text/html',this.innerHTML);
+
+}
+
+function handleDragEnter(e){
+    console.log('handleDragEnter');
+
+    if(this!==curentDragItem && curentDragItem !=null){
+        // this.classList.add('drag-over');
+    }
+
+
+}
+
+function handleDragOver(e){
+    console.log('handleDragOver');
+
+    if(e.preventDefault){
+        e.preventDefault();
+    }
+    e.dataTransfer.dropEffect='move';
+    return false;
+}
+
+function handleDragLeave(e){
+    this.classList.remove('drag-over');
+}
+
+function handleDragEnd(e){
+    dragItems.forEach(item=>{
+        item.classList.remove('drag-over');
+    })
+}
+function handleDragDrop(e){
+    e.stopPropagation();
+    if(this!==curentDragItem && curentDragItem !=null){
+        curentDragItem.innerHTML=this.innerHTML;
+        this.innerHTML=e.dataTransfer.getData('text/html');
+    }
+}
+
+dragItems.forEach(item=>{
+    item.addEventListener('dragstart', handleDragStart, false);
+    item.addEventListener('dragenter', handleDragEnter, false);
+    item.addEventListener('dragover', handleDragOver, false);
+    item.addEventListener('dragleave', handleDragLeave, false);
+    item.addEventListener('dragend', handleDragEnd, false);
+    item.addEventListener('drop', handleDragDrop, false);
+})
